36f1270fd1bca9309e86180d8def2c679feaef01
kauramanp <33446281+kauramanp@users.noreply.github.com>
2023-09-08 23:15:10 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# NotesCompose
+ 
---
+++ app/src/main/java/com/amanpreet/notescompose/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.amanpreet.notescompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.amanpreet.notescompose.ui.theme.NotesComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NotesComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    NotesComposeTheme {
+        Greeting("Android")
+    }
+}
