6adcb4d35b9b86b90f17c011781029979788e8ed
kauramanp <33446281+kauramanp@users.noreply.github.com>
2023-09-18 23:43:21 +0530

    fab button click updated list

--- app/src/main/java/com/amanpreet/notescompose/MainActivity.kt
+++ app/src/main/java/com/amanpreet/notescompose/MainActivity.kt
@@ -1,18 +1,30 @@
 package com.amanpreet.notescompose
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.*
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.amanpreet.notescompose.ui.theme.NotesComposeTheme
+import androidx.compose.runtime.mutableStateOf
 
 class MainActivity : ComponentActivity() {
+    var list = arrayListOf<String>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,7 +34,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                 //   Greeting("Android")
+                    ScaffoldExample()
                 }
             }
         }
@@ -31,16 +44,59 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
+   /* Text(
         text = "Hello $name!",
         modifier = modifier
-    )
+    )*/
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ScaffoldExample() {
+    val presses: MutableState<Int> = remember { mutableStateOf(1) }
+
+    val list = remember {
+        mutableStateListOf<String>("item1", "item2", "item3")
+
+    }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text("Array List CRUD")
+                }
+            )
+        },
+
+        floatingActionButton = {
+            FloatingActionButton(onClick = {
+                presses.value +=1
+                list.add("Testing ${presses.value}")
+                println("Clicked ${list.size}")
+
+            }) {
+                Icon(Icons.Default.Add, contentDescription = "Add")
+            }
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier.padding(innerPadding),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+        ) {
+            for(items in list){
+                Text(  modifier = Modifier.padding(8.dp),
+                   text =  "$items")
+
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     NotesComposeTheme {
-        Greeting("Android")
+//        Greeting("Android")
+        ScaffoldExample()
     }
 }
